ac0cb01d0ee8ed0aedf65b946ae1e32669abf6b7
Lucmaret <baptistelucmaret@wanadoo.fr>
2020-11-26 18:48:09 +0100

    Séparation en 2 Controller, AdminArticlesController et ArticlesController

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -4,12 +4,8 @@
 namespace App\Controller;
 
 
-use App\Entity\Article;
-use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticlesController extends AbstractController
@@ -33,148 +29,23 @@ class ArticlesController extends AbstractController
         ]);
     }
 
-
+    // Je créé une url avec une wildcard "id" qui contiendra l'id d'un article.
     /**
      * @Route("/article/show/{id}", name="article_show")
      */
+    // en parametre de la méthode, je récupère la valeur de la wildcard id
+    // et je demande en plus à symfony d'instancier pour moi
+    // la classe ArticleRepository dans une variable $articleRepository
+    // (autowire)
+
     public function articleShow($id, ArticleRepository $articleRepository)
     {
+        // J'utilise l'ArticleRepository avec la méthode find pour faire
+        // une requête SQL SELECT en BDD et retrouver l'article dont l'id correspond à l'id passé en URL
         $articles = $articleRepository->find($id);
 
-        // en parametre de la méthode, je récupère la valeur de la wildcard id
-        // et je demande en plus à symfony d'instancier pour moi
-        // la classe ArticleRepository dans une variable $articleRepository
-        // (autowire)
         return $this->render('article.html.twig', [
             'article' => $articles
         ]);
-    }
-
-    /**
-     * @Route("article/insert", name="article_insert")
-     */
-
-    public function insertArticle(Request $request, EntityManagerInterface $entityManager)
-    {
-        // Je créé une nouvelle instance de l'Entité Article
-        // pour créer un nouvel enregstrement en bdd et pour pouvoir le lier à mon formulaire.
-        $article = new Article();
-
-
-        // Je veux afficher mon formulaire ArticleType pour créer des articles.
-        // Dans les paramètres de la méthode createForm de l'AbstractController j'indique
-        // mon gabarit de formulaire ArticleType pour le récupérer (créé en ligne de commandes)
-        // et en indiquant son chemin (::class).
-
-        // Je lie mon formulaire à l'article.
-        $form = $this->createForm(ArticleType::class, $article);
-
-
-        // Après avoir lié mon formulaire que j'ai créé au préalable, je peux récupèrer
-        // les données saisies lors du remplissage du formulaire grâce à la classe Request
-        // instanciée dans une variable $request. Cette variable applique la méthode POST.
-        // De cette manière, je pourrai utiliser la variable $form pour vérifier si
-        // les données POST ont été envoyées ou pas.
-
-        $form->handleRequest($request);
-
-        // Si le form a été envoyé et qu'il est valide, je pré-sauvegarde et j'envoie à la BDD
-        // les données saisies dans le formulaire.
-        if ($form->isSubmitted() && $form->isValid()) {
-            // alors je l'enregistre en BDD
-            $entityManager->persist($article);
-            $entityManager->flush();
-
-            $this->addFlash(
-                "success",
-                "ARTICLE BIEN AJOUTÉ"
-            );
-
-            return $this->redirectToRoute('articles_list');
-        }
-
-        // Je prends le gabarit de formulaire récupéré et je créé une "vue" de formulaire
-        // pour pouvoir l'afficher dans mon fichier twig
-        $formView = $form->createView();
-
-        // J'envoie la vue de mon formulaire à twig
-        return $this->render('insert.html.twig', [
-            'formView' => $formView
-        ]);
-    }
-
-
-    /**
-     * @Route("article/update/{id}", name="article_update")
-     */
-
-    public function updateArticle(
-        $id,
-        ArticleRepository $articleRepository,
-        Request $request,
-        EntityManagerInterface $entityManager)
-    {
-        $article = $articleRepository->find($id);
-
-        if (is_null($article)) {
-            return $this->redirectToRoute('articles_list');
-        }
-
-        $form = $this->createForm(ArticleType::class, $article);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($article);
-            $entityManager->flush();
-
-            $this->addFlash(
-                "success",
-                "ARTICLE BIEN MODIFIÉ"
-            );
-
-            return $this->redirectToRoute('articles_list');
-        }
-
-        $formView = $form->createView();
-
-        return $this->render('update.html.twig', [
-            'formView' => $formView
-        ]);
-    }
-
-    /**
-     * @Route("article/delete/{id}", name="article_delete")
-     */
-
-    // Je récupère la wildcard de l'url dans le parametre $id.
-    // Je demande à SF d'instancier les classes ArticleRepository et
-    // EntityManager (autowire).
-
-    public function deleteArticle($id, ArticleRepository $articleRepository, EntityManagerInterface $entityManager)
-    {
-        // Je récupère en BDD l'article dont l'id correspond à celui passé en url (wildcard)
-        // pour récupérer l'article en BDD qui correspond à cette wildcard sous forme de
-        // requête SQL Select avec un filtre where id.
-
-        $article = $articleRepository->find($id);
-
-        // Si cet article existe en bdd (donc si la valeur de ma variable $article n'est pas "null")
-        // alors je le supprime avec la méthode remove de l'EntityManager.
-        if (!is_null($article)) {
-            $entityManager->remove($article);
-            $entityManager->flush();
-
-            // Si j'ai bien supprimé mon article,
-            // j'ajoute un message flash de type "succès"
-            // et je lui définis un message
-            $this->addFlash(
-                "success",
-                "ARTICLE BIEN SUPPRIME"
-            );
-        }
-
-        // Je fais une redirection vers ma page liste d'article une fois la suppression faite.
-        return $this->redirectToRoute('articles_list');
     }
 }
