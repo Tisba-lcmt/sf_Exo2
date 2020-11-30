da55b91c46d0cf89f46816868ec6e65dc01320ea
Lucmaret <baptistelucmaret@wanadoo.fr>
2020-11-30 17:17:48 +0100

    Insertion d'un input category dans le insert et update article twig form

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -24,6 +26,22 @@ class ArticleType extends AbstractType
             ])
             ->add('creationDate', DateType::class, [
                 'widget' => 'single_text'
+            ])
+            // Dans mon gabarit de formulaire ArticleType, je créé un input category
+            // relié à ma propriété category dans l'entité Article.
+            // Vu que c'est une relation vers une autre entité (une autre table) je lui passe
+            // en type "EntityType" pour décrire que c'est de type Entité
+            ->add('category', EntityType::class, [
+                // Je précise que c'est bien dans l'Entity Category que je veux affiche
+                // mon input et donc je pourrais afficher toutes les catégories sous forme
+                // de liste déroulante.
+                'class' => Category::class,
+                // Dans la liste déroulante, je choisirai la propriété à afficher pour
+                // chacune des catégories (il faut qu'elle permette à l'utilisateur
+                // d'identifer et de choisir une catégorie).
+                // ici c'est le titre qui est obligatoire dans la saisie et qui identifie
+                // la catégorie.
+                'choice_label' => 'title'
             ])
             ->add('isPublished')
             ->add('Envoyer', SubmitType::class)
